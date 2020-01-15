36da771cf4e5dd30cf59354163cfe7fbda47b7fb
Murchison <Will.Murchison@AnchorGlass.com>
2020-01-15 13:27:03 -0500

    updated

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+dotnet publish -c Release 
+
+xcopy dockerfile ./bin/release/netcoreapp2.2/publish
+
+docker build -t test_heroku-image ./bin/release/netcoreapp2.2/publish
+
+docker tag test_heroku-image registry.heroku.com/testanchorheroku/web
+
+docker push registry.heroku.com/testanchorheroku/web
+
+heroku container:release web -a testanchorheroku
